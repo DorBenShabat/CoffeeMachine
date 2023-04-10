3e8c1016af0504b7d50dd15aca608e987739637b
dor ben shabat <dorbs201@gmail.com>
2023-04-11 00:06:32 +0300

    add

--- coffee_machine.py
+++ coffee_machine.py
@@ -15,7 +15,7 @@ class CoffeeMachine:
             if choice in self.mapping:
                 price = int(self.mapping[choice])  # Brings the price of the drink.
                 choice = input("Would you like something else? Y/N: ")
-                if choice.upper() == "N": # Second choice is not in the menu.
+                if choice.upper() == "N":  # User don't want another drink.
                     payment = self.process_payment(price)
                     if payment >= price:
                         self.make_coffee()
@@ -23,13 +23,13 @@ class CoffeeMachine:
                 elif choice.upper() == "Y":  # User wants another drink.
                     sec_choice = input("Please choose another drink: ")
                     if sec_choice in self.mapping:
-                        price += int(self.mapping[sec_choice])  # Calculates new price of the product.
+                        price += int(self.mapping[sec_choice])  # Calculates new price.
                         payment = self.process_payment(price)
-                        if payment >= price:  # If enough money, make coffee.
+                        if payment >= price:  # If the payment is enough, make coffee.
                             self.make_coffee()
                             is_off = True
                 else:
-                    print("Your choice is incorrect please run the program again")
+                    print("Your choice is incorrect")
 
             else:
                 is_off = True
@@ -45,8 +45,10 @@ class CoffeeMachine:
         while not is_off:
             if customer_payment < price:
                 customer_payment += int(input(f"You need to add: {price - customer_payment}\nEnter more coins: "))
-            elif customer_payment >= price:
+            elif customer_payment > price:
                 print(f"Thank you! Here is your excess: {customer_payment - price}")
+                is_off = True
+            elif customer_payment == price:
                 is_off = True
         return customer_payment
 
